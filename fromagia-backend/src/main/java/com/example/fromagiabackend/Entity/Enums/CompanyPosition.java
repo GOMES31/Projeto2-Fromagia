06c5e5cb3f072c902dd0c58f8cf7ab911a203169
Eduardo Gomes <dugomes02@gmail.com>
2024-07-08 17:01:52 +0100

    added styles to register page

--- fromagia-backend/src/main/java/com/example/fromagiabackend/Entity/Enums/CompanyPosition.java
+++
@@ -1,6 +0,0 @@
-package com.example.fromagiabackend.Entity.Enums;
-
-public enum CompanyPosition {
-    PRODUCER,
-    MANAGER
-}
--- fromagia-backend/src/main/java/com/example/fromagiabackend/Service/Supplier/SupplierService.java
+++
@@ -1,8 +0,0 @@
-package com.example.fromagiabackend.Service.Supplier;
-
-import com.example.fromagiabackend.Entity.Supplier;
-
-public interface SupplierService {
-
-    void save(Supplier supplier);
-}
