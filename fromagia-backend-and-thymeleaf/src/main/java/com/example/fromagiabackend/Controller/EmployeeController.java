c41e66ee094e35cf45f3d6ee29ae5b9f45e3ffa9
Eduardo Gomes <dugomes02@gmail.com>
2024-07-13 02:16:56 +0100

    added employees pages

--- fromagia-backend-and-thymeleaf/src/main/java/com/example/fromagiabackend/Controller/EmployeeController.java
+++ fromagia-backend-and-thymeleaf/src/main/java/com/example/fromagiabackend/Controller/EmployeeController.java
@@ -2,8 +2,12 @@ package com.example.fromagiabackend.Controller;
 
 
 import com.example.fromagiabackend.Entity.Company;
+import com.example.fromagiabackend.Entity.Employee;
 import com.example.fromagiabackend.Entity.Enums.AccountType;
+import com.example.fromagiabackend.Entity.Enums.CompanyPosition;
+import com.example.fromagiabackend.Entity.Order;
 import com.example.fromagiabackend.Entity.User;
+import com.example.fromagiabackend.Service.Company.CompanyService;
 import com.example.fromagiabackend.Service.Product.ProductService;
 import com.example.fromagiabackend.Service.User.UserService;
 import jakarta.servlet.http.HttpSession;
@@ -14,22 +18,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.List;
 import java.util.Objects;
 
 import static com.example.fromagiabackend.Controller.AppController.getHomeRedirectUrl;
 
 @Controller
-@RequestMapping("/employee")
+@RequestMapping("/employees")
 public class EmployeeController {
 
     private final UserService userService;
     private final ProductService productService;
 
+    private final CompanyService companyService;
+
     @Autowired
     public EmployeeController(UserService _userService,
-                              ProductService _productService){
+                              ProductService _productService,
+                              CompanyService _companyService){
         userService = _userService;
         productService = _productService;
+        companyService = _companyService;
     }
     @GetMapping("/home")
     public String showHomePage(Model model, HttpSession session, RedirectAttributes redirectAttributes){
@@ -43,6 +52,49 @@ public class EmployeeController {
         model.addAttribute("user",currentUser);
         return "employees/home";
     }
+
+
+    @GetMapping("/orders")
+    public String showOrdersPage(Model model, HttpSession session, RedirectAttributes redirectAttributes){
+        User currentUser = (User) session.getAttribute("user");
+
+        String authenticationResult = handleUserAuthenticationAndPermissions(currentUser, redirectAttributes);
+        if (authenticationResult != null) {
+            return authenticationResult;
+        }
+
+        CompanyPosition positionAllowed = CompanyPosition.MANAGER;
+
+        String employeePermissionsResult = handleEmployeePermissions(currentUser, redirectAttributes, positionAllowed);
+        if( employeePermissionsResult != null){
+            return employeePermissionsResult;
+        }
+
+
+        Employee employee = currentUser.getEmployee();
+
+        Company employeeCompany = employee.getCompany();
+
+        List<Order> orders = companyService.getCompanyOrders(employeeCompany.getId());
+
+        model.addAttribute("employee",employee);
+        model.addAttribute("orders",orders);
+
+        return "employees/orders";
+    }
+
+    private String handleEmployeePermissions(User currentUser, RedirectAttributes redirectAttributes, CompanyPosition companyPosition) {
+
+        CompanyPosition userPosition = currentUser.getEmployee().getCompanyPosition();
+
+        if(!Objects.equals(userPosition,companyPosition)){
+            redirectAttributes.addFlashAttribute("error", "Não tens permissões para aceder a esta página!");
+            return getHomeRedirectUrl(currentUser);
+        }
+
+        return null;
+    }
+
 
     private String handleUserAuthenticationAndPermissions(User currentUser, RedirectAttributes redirectAttributes) {
 
