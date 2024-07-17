383f69e5a4f9884b31cb27fcc43a448d26bb38ae
Eduardo Gomes <dugomes02@gmail.com>
2024-07-17 17:35:18 +0100

    last version

--- fromagia-backend-and-thymeleaf/src/main/java/com/example/fromagiabackend/Service/Invoice/InvoiceService.java
+++ fromagia-backend-and-thymeleaf/src/main/java/com/example/fromagiabackend/Service/Invoice/InvoiceService.java
@@ -2,7 +2,11 @@ package com.example.fromagiabackend.Service.Invoice;
 
 import com.example.fromagiabackend.Entity.Invoice;
 
+import java.util.Optional;
+
 public interface InvoiceService {
 
     void save(Invoice invoice);
+
+    Optional<Invoice> findByOrderId(Integer id);
 }
